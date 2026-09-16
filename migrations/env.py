39c7157bf2afcd326a4869bbb0f2f80abec95e24
tarabk <anbojela@gmil.com>
2026-09-16 09:29:17 +0300

    fix: escape percent signs in Alembic database URL

--- migrations/env.py
+++ migrations/env.py
@@ -10,7 +10,10 @@ from app import models  # noqa: F401
 config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-config.set_main_option("sqlalchemy.url", get_settings().database_url)
+config.set_main_option(
+    "sqlalchemy.url",
+    get_settings().database_url.replace("%", "%%"),
+)
 target_metadata = Base.metadata
 
 
